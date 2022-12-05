fa2ad54d0551b1f1f08b972c10c52f7447bac732
levi-kennedy <levi@kennedykin.com>
2022-12-05 00:04:19 -0500

    define utility skeletons, add requirements doc and gitignore doc

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import ffmpeg
+def get_video(video_file_path):
+    {
+        """ 
+        Read in video data into an FFMPeg object
+        input:
+        (string) video file path
+        output:
+        (ffmpeg object) ffmpeg video object
+        """
+    }
+
+def display_video_metadata(video):
+    {
+        """
+        Extract and print the video file data in a pretty format for inspection
+        input:
+        (ffmpeg object) video object
+        output:
+        none
+        """
+    }
+
+def video_metadata_to_dict(video):
+    {
+        """
+        Extract video metadata from a video file and output to a python dict
+        input:
+        (ffmpeg object) video object
+        output: 
+        (dict) metadata dictionary of provided file
+        """
+    }
+
+def dict_to_video_metadata(vid_metadata):
+    {
+        """
+        Receives a dict and inserts it back in the video object
+        input:
+        (dict) video metadata dictionary to insert into video
+        output:
+        (ffmpeg object) video object with new metadata
+        """
+    }
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
